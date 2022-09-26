c5d0aaad770ff092ccbbab89a84a1a4ca82260a1
Jiheng Nie <Niejiheng@gmail.com>
2022-09-26 22:55:45 +0000

    Added in the ability to truncate text in search results

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var $mobileSearch = document.querySelector('.mobile-search-hidden');
 var $windowSearch = document.querySelector('.window-search-hidden');
 var windowCurrentSize = window.innerWidth;
+var $listOfCardTexts = document.querySelectorAll('.truncate');
 
 window.addEventListener('resize', searchBarDisplay);
 
+// Used for showing the correct search bar based on window size change
 function searchBarDisplay(event) {
   if (window.innerWidth > 768) {
     $mobileSearch.className = 'hidden';
@@ -14,6 +16,7 @@ function searchBarDisplay(event) {
   }
 }
 
+// Used for showing the correct search bar based on current window size
 if (windowCurrentSize > 768) {
   $mobileSearch.className = 'hidden';
   $windowSearch.className = 'column-one-third search-bar-background window-search-hidden';
@@ -21,3 +24,23 @@ if (windowCurrentSize > 768) {
   $windowSearch.className = 'hidden';
   $mobileSearch.className = 'column-one-third search-bar-background window-search-hidden';
 }
+
+// Used for truncating texts
+function truncateTexts() {
+  var threeLines = 200;
+  for (var i = 0; i < $listOfCardTexts.length; i++) {
+    if ($listOfCardTexts[i].textContent.length >= threeLines) {
+      $listOfCardTexts[i].textContent = $listOfCardTexts[i].textContent.slice(0, threeLines) + '...';
+    }
+  }
+}
+truncateTexts();
+
+function getYugiohData(cardName) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://db.ygoprodeck.com/api/v7/cardinfo.php' + 'name=' + cardName);
+  xhr.responseType = 'json';
+  xhr.send();
+}
+
+getYugiohData('Kuriboh');
