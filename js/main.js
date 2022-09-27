329a6f37b78e76934beb28b1ae414ed8ebec9499
Jiheng Nie <Niejiheng@gmail.com>
2022-09-27 23:51:33 +0000

    Started on single card view

--- js/main.js
+++ js/main.js
@@ -28,6 +28,26 @@ function resetSearchResults() {
   }
 }
 
+// Used for exact name search
+// function getYugiohDataExact(exactCardName) {
+//   var tempData;
+//   var tempDomTree;
+//   var targetUrl = encodeURIComponent('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + exactCardName);
+//   var xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
+//   xhr.setRequestHeader('token', 'abc123');
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', function () {
+//     tempData = xhr.response;
+//     tempDomTree = generateSearchCard(tempData);
+//     $searchResultFeed.appendChild(tempDomTree);
+//   });
+//   xhr.send();
+// }
+
+// getYugiohDataExact('Blue-Eyes White Dragon');
+// getYugiohDataExact('Kuriboh');
+
 function getYugiohDataFuzzy(fuzzyCardName) {
   var tempData;
   var tempDomTree;
